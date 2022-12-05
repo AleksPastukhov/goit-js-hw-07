64e25d358d49e6acff2bd7a4ca2cdde27688080e
AleksPastukhov <passandro1990@gmail.com>
2022-12-05 21:21:50 +0200

    adds a solution to the second part of the task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+const galleryMarkap = createGaleryMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML('beforeend', galleryMarkap);
+
+galleryBox.addEventListener('click', onImageOfGalleryClick);
+
+function createGaleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      image => `
+<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+`
+    )
+    .join('');
+}
+
+function onImageOfGalleryClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const originalImage = evt.target.dataset.source;
+}
