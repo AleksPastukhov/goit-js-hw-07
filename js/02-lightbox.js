b4199ef6bf38ccecd5a9ea61232773a14d639de5
AleksPastukhov <passandro1990@gmail.com>
2022-12-06 11:22:46 +0200

    add SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryBox = document.querySelector('.gallery');
 const galleryMarkap = createGaleryMarkup(galleryItems);
 
@@ -14,9 +12,10 @@ function createGaleryMarkup(galleryItems) {
   return galleryItems
     .map(
       image => `
-<a class="gallery__item" href="${image.original}">
-  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
-</a>
+    <a class="gallery__item" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" 
+ />
+    </a>
 `
     )
     .join('');
@@ -27,5 +26,7 @@ function onImageOfGalleryClick(evt) {
   if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
-  const originalImage = evt.target.dataset.source;
+
+  let gallerySet = new SimpleLightbox('.gallery a');
+  gallerySet.on('show.simplelightbox', function () {});
 }
